7e8e1d99cbfcafdd54d16bcd29ac5f1c4c002db7
Rafael Jesus <rafaelljesus86@gmail.com>
2016-05-14 21:24:55 -0300

    Implement one more spec

--- test/aws-spec.js
+++ test/aws-spec.js
@@ -9,27 +9,37 @@ import {
 
 test.before(() => {
   nock('https://elasticloadbalancing.us-east-1.amazonaws.com')
-  .post('/', (body) => true)
+  .post('/', '*')
   .reply(200, {
     LoadBalancerDescriptions: 'foo_elb_name',
     Subnets: ['sub-net-pub', 'sub-net-pvt']
+  })
+  nock('https://elasticloadbalancing.us-east-1.amazonaws.com')
+  .post('/', '*')
+  .reply(200, {
+    Reservations: [{
+      Instances: [
+        { InstanceId: 1 },
+        { InstanceId: 2 }
+      ]
+    }]
   })
 })
 
 test.after(nock.cleanAll)
 
-test('.describeLoadBalancers', async (t) => {
+test('should describe load balancers', async (t) => {
   const subnets = await describeLoadBalancers()
   t.truthy(subnets.length === 2)
 })
 
-test.skip('.describeInstances', async (t) => {
-  const fakeSubnets = [{}]
+test('should describe instances', async (t) => {
+  const fakeSubnets = ['sub-net-pub', 'sub-net-pvt']
   const instances = await describeInstances(fakeSubnets)
-  t.truthy(instances)
+  t.truthy(instances.length === 2)
 })
 
-test.skip('.registerInstancesWithLoadBalancer', async (t) => {
+test.skip('should register instances with load balancer', async (t) => {
   const fakeInstances = [{}]
   const res = await registerInstancesWithLoadBalancer(fakeInstances)
   t.truthy(res)
